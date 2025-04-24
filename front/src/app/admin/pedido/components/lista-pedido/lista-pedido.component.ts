6305d17a5560c37792dec4de8f20ea5910abb6bf
ElZero002 <jarystian@gmail.com>
2025-04-23 22:50:02 -0500

    Clase 25

--- front/src/app/admin/pedido/components/lista-pedido/lista-pedido.component.ts
+++ front/src/app/admin/pedido/components/lista-pedido/lista-pedido.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { PedidoService } from '../../service/pedido.service';
+import jsPDF from 'jspdf';
+import autotable from 'jspdf-autotable';
+import { Cliente } from '../../../../../../../Back/src/modules/cliente/entities/cliente.entity';
 
 @Component({
   selector: 'app-lista-pedido',
@@ -86,4 +89,42 @@ export class ListaPedidoComponent {
     this.mostrar_pedido = true;
     this.detalle_pedido = pedido;
   }
+
+  generarPDF(pedido: any): void {
+    // Probamos si genera
+    const doc = new jsPDF();
+    doc.setFontSize(18);  //tamaño de la fuente 
+    doc.text("Recibo del pedido", 10, 10);  //titilo de la posicion x y 
+
+    doc.setFontSize(12);
+    doc.text('Número de Pedido'+pedido.id, 10,30);
+    doc.text('Fecha# Pedido:' +pedido.fecha,15,40);
+
+    doc.setFontSize(13);
+    doc.text('Datos de Cliente',10,50);
+    doc.text('Cliente: ' +pedido.cliente.nombre_completo, 15,60);
+    doc.text('DNI: ' +pedido.cliente.dni, 15,70);
+
+    const headers = [['Producto', 'Precio', 'Cantidad', 'Subtotal']]
+    const productos=pedido.productos.map((produ: any)=>{
+      const subtotal =parseFloat(produ.precio) * productos.cantidad 
+
+      return [
+        produ.nombre,
+        `$ ${produ.precio}`,
+        produ.cantidad,
+        `$ ${subtotal.toFixed(2)}`,
+
+      ]
+    })
+
+    const startY =80
+    autotable(doc,{
+      head: headers,
+      body: productos,
+      startY: startY,
+    })
+    doc.save("recibo.pdf");
+  }
+  
 }
